b2d9646a0d89bfc91025caa71fdf67010b8cd15f
noy2611 <noyr2611@gmail.com>
2024-07-05 15:05:15 +0300

    create api posts

--- frontend/src/apis/posts.api.js
+++ frontend/src/apis/posts.api.js
@@ -1,11 +1,40 @@
+// import axios from 'axios';
+
+// const localURL = "http://localhost:5000";
+
+// export const userLogin = async (userInfo) => {
+//     return await axios.post(`${localURL}/api/users/login/`, userInfo);
+// }
+
+// export const getUser = async (userInfo) => {
+//     return await axios.post(`${localURL}/api/users`, userInfo);
+// }
+
 import axios from 'axios';
 
-const localURL = "http://localhost:5000";
+const localURL = "http://localhost:8000";
 
-export const userLogin = async (userInfo) => {
-    return await axios.post(`${localURL}/api/users/login/`, userInfo);
-}
+export const getPosts = async () => {
+    try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            throw new Error("Token not found");
+        }
 
-export const getUser = async (userInfo) => {
-    return await axios.post(`${localURL}/api/users`, userInfo);
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        };
+
+        const response = await axios.get(`${localURL}/api/posts/`, config);
+        if (response.status === 200) {
+            return response.data;
+        } else {
+            throw new Error(`Error: ${response.status}`);
+        }
+    } catch (error) {
+        console.error(`Failed to get posts: ${error}`);
+        throw error;
+    }
 }
