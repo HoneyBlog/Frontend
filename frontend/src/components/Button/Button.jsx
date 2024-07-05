999c3bf31f02a0b14f48b767cb3e0eb01279b744
Adi Levi <adile0194@gmail.com>
2024-07-05 15:37:49 +0300

    updated button style

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -4,9 +4,9 @@ import { ButtonContainer } from './Button.styled';
 const Button = ({ text, paddingUpDown, paddingRightLeft, fontSize }) => {
   return (
     <ButtonContainer 
-      $fontSize={fontSize} 
-      $paddingUpDown={paddingUpDown} 
-      $paddingRightLeft={paddingRightLeft}
+      fontSize={fontSize} 
+      paddingUpDown={paddingUpDown} 
+      paddingRightLeft={paddingRightLeft}
     >
       {text}
     </ButtonContainer>
--- frontend/src/components/Button/Button.styled.js
+++ frontend/src/components/Button/Button.styled.js
@@ -1,19 +1,20 @@
 import styled from "styled-components";
 
-
-export const ButtonContainer = styled.button`
-    background-color: #bea6d8;
-    color: #FFFFFF;
-    border-radius: 30px;
-    font-weight: 600;
-    border: none;
-    cursor: pointer;
-    transition: 0.2s;
-    font-size: ${(props) => props.fontSize || '1.3rem'};
-    padding: ${(props) => props.paddingUpDown || '15px'} ${(props) => props.paddingRightLeft || '40px'}; 
-
-    &:hover {
-        background-color: #FFFFFF;
-        color: #bea6d8;
-    }
-`;
+export const ButtonContainer = styled.button.withConfig({
+    shouldForwardProp: (prop) => !['fontSize', 'paddingUpDown', 'paddingRightLeft'].includes(prop),
+  })`
+      background-color: #bea6d8;
+      color: #FFFFFF;
+      border-radius: 30px;
+      font-weight: 600;
+      border: none;
+      cursor: pointer;
+      transition: 0.2s;
+      font-size: ${(props) => props.fontSize || '1.3rem'};
+      padding: ${(props) => props.paddingUpDown || '15px'} ${(props) => props.paddingRightLeft || '40px'}; 
+  
+      &:hover {
+          background-color: #FFFFFF;
+          color: #bea6d8;
+      }
+  `;
